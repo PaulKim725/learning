705982b16cb447d247d31499e9a84f67936c5c31
小志725 <xiaozhi725@hotmail.com>
2020-11-14 09:42:35 +0800

    mybatis generator

---
+++ spring-boot-4/src/test/java/com/paul/springboot4/SpringBoot4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paul.springboot4;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
